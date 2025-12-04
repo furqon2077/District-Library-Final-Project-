ba54055eb4250424565f3908ac2e39ac97f7337c
furqon <furqon@mail.ru>
2025-12-04 16:52:21 +0500

    finalized and deployed

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: "/District-Library-Final-Project-/", // <--- ADD THIS
+
     plugins: [react()],
+
     resolve: {
         alias: {
             "@": "/src",
         },
     },
 });
+
 
